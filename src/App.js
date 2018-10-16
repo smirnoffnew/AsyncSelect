51d9e47c2c37db4009d4b46a72297e3bae3589f8
Alex <agent150891@gmail.com>
2018-10-17 01:23:33 +0300

    AsyncSelect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AsyncSelect from 'react-select/lib/Async';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.colourOptions = [
+            {label: 'red', value: 'red'},
+            {label: 'aqua', value: 'aqua'},
+            {label: 'black', value: 'black'},
+            {label: 'blue', value: 'blue'},
+            {label: 'fuchsia', value: 'fuchsia'},
+            {label: 'gray', value: 'gray'},
+            {label: 'green', value: 'green'},
+            {label: 'lime', value: 'lime'},
+            {label: 'maroon', value: 'maroon'},
+            {label: 'navy', value: 'navy'},
+            {label: 'olive', value: 'olive'},
+            {label: 'orange', value: 'orange'},
+            {label: 'purple', value: 'purple'},
+        ];
+
+        this.state = {
+            inputValue: '',
+            selectedValue: this.colourOptions[0]
+        };
+    }
+
+    filterColors = (inputValue) => this.colourOptions.filter(i => i.label.toLowerCase().includes(inputValue.toLowerCase()));
+
+    loadOptions = (inputValue, callback) => {
+        setTimeout(() => {
+            callback(this.filterColors(inputValue));
+        }, 1000);
+    };
+
+    handleInputChange = (newValue) => {
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                inputValue: newValue
+            }
+        });
+        return newValue;
+    };
+
+
+    onChange = (selected, action) => {
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                selectedValue: selected
+            }
+        });
+        console.log('selected', selected);
+        console.log('action', action);
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        select your color
+                    </p>
+                    <div style={{color: '#000'}}>
+                        <pre>inputValue: "{this.state.inputValue}"</pre>
+                        <pre style={{color: this.state.selectedValue.value}}>
+                            selectedValue: "{this.state.selectedValue.value}"
+                        </pre>
+                        <AsyncSelect
+                            cacheOptions
+                            loadOptions={this.loadOptions}
+                            defaultOptions={[{label: 'hui vam', value: 'hui vam a ne cvet'}]}
+                            onInputChange={this.handleInputChange}
+                            onChange={this.onChange}
+                            value={this.state.selectedValue}
+                        />
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
